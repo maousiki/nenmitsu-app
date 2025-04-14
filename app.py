84ef1d439d54133a37926f6fd9495c6b30888055
maousiki <maousiki@yahoo.co.jp>
2025-04-14 20:33:06 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+import pandas as pd
+
+def convert_time_to_minutes(time_str):
+    ...
+def process_csv_data(uploaded_file):
+    ...
+def main():
+    ...
+if __name__ == "__main__":
+    main()
